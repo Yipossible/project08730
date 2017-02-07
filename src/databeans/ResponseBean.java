40e1ee011688d8cf4691d09efbad1802a33e1b32
Yipossible <yi.wang.carnegie.mellon@gmail.com>
2017-02-06 23:25:31 -0500

    start to build MVC with new pages and classes created

---
+++ src/databeans/ResponseBean.java
@@ -0,0 +1,40 @@
+package databeans;
+
+import org.genericdao.PrimaryKey;
+
+@PrimaryKey("response_id")
+public class ResponseBean {
+	private int response_id;
+	private int question_id;
+	private int respondent_id;
+	private String response;
+	
+	
+	public int getResponse_id() {
+		return response_id;
+	}
+	public void setResponse_id(int response_id) {
+		this.response_id = response_id;
+	}
+	public int getQuestion_id() {
+		return question_id;
+	}
+	public void setQuestion_id(int question_id) {
+		this.question_id = question_id;
+	}
+	public int getRespondent_id() {
+		return respondent_id;
+	}
+	public void setRespondent_id(int respondent_id) {
+		this.respondent_id = respondent_id;
+	}
+	public String getResponse() {
+		return response;
+	}
+	public void setResponse(String response) {
+		this.response = response;
+	}
+	
+	
+	
+}
---
+++ src/formbean/Question18Form.java
@@ -0,0 +1,9 @@
+package formbean;
+
+import org.mybeans.form.FormBean;
+
+public class Question18Form extends FormBean{
+
+	
+	
+}
